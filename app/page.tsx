458a2ff4a00169ffced84f1fb9b2e32327d7a977
Philip Müller <philipmuller97@gmail.com>
2023-12-08 10:24:00 +0200

    Adaptive layout with flex basis

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,22 @@ import Spline from '@splinetool/react-spline';
 import { Horse, Heart, Cube } from '@phosphor-icons/react';
 
 export default function Home() {
+
+  function onScroll() {
+    //console.log('onScroll');
+  }
+
   return (
     <main className="flex flex-col min-h-screen items-center justify-between bg-[#5B5540]">
-      <div className="w-full h-full p-8 flex flex-col gap-6">
+      <div className="w-full h-full p-8 flex basis-1 flex-1 flex-col gap-6">
 
-        <div className="w-full h-[80vh] items-center justify-between font-mono text-sm bg-white bg-red-500 overflow-hidden rounded-2xl">
-          <Spline scene="https://prod.spline.design/vOTSy200mmfodOdw/scene.splinecode"/> 
+        <div className="w-full items-center grow shrink justify-between font-mono text-sm overflow-hidden rounded-2xl">
+          <Spline onScroll={onScroll} scene="https://prod.spline.design/vOTSy200mmfodOdw/scene.splinecode"/>
         </div>
 
-        <div className='flex flex-col px-6 z-10 gap-1'>
-          <h1 className='text-xl font-semibold tracking-widest uppercase'>Organon</h1>
-          <p className='text-sm font-light opacity-80'>Write and learn formal logic like never before, designed for iPhone.</p>
+        <div className='flex flex-col px-6 pb-10 z-10 gap-1'>
+          <h1 className='text-lg font-medium tracking-widest uppercase'>Organon</h1>
+          <p className='text-xs font-light opacity-80'>Write and learn formal logic like never before, designed for iPhone.</p>
         </div>
 
       </div>
