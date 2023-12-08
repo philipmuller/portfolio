f68628e1f47053dd8d39f7bdc1ad4eb5610f2ee6
Philip Müller <philipmuller97@gmail.com>
2023-12-08 11:05:57 +0200

    Fix layout in safari

--- app/page.tsx
+++ app/page.tsx
@@ -12,15 +12,16 @@ export default function Home() {
 
   return (
     <main className="flex flex-col min-h-screen items-center justify-between bg-[#5B5540]">
-      <div className="w-full h-full p-8 flex basis-1 flex-1 flex-col gap-6">
+      <div className="w-full min-h-screen h-screen p-8 flex flex-col gap-6">
 
-        <div className="w-full items-center grow shrink justify-between font-mono text-sm overflow-hidden rounded-2xl">
+        <div className="w-full basis-5/6 items-center justify-between font-mono text-sm overflow-hidden rounded-2xl">
           <Spline onScroll={onScroll} scene="https://prod.spline.design/vOTSy200mmfodOdw/scene.splinecode"/>
+          <p className="text-white">Hello World</p>
         </div>
 
-        <div className='flex flex-col px-6 pb-10 z-10 gap-1'>
+        <div className='flex flex-col basis-1/6 px-6 pb-10 z-10 gap-1'>
           <h1 className='text-lg font-medium tracking-widest uppercase'>Organon</h1>
-          <p className='text-xs font-light opacity-80'>Write and learn formal logic like never before, designed for iPhone.</p>
+          <p className='text-sm font-light opacity-80'>Write and learn formal logic like never before, designed for iPhone.</p>
         </div>
 
       </div>
