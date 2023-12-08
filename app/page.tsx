b406637793025cba244d577841ae1f7be4b8e933
Philip Müller <philipmuller97@gmail.com>
2023-12-08 17:12:40 +0200

    Multiple cards

--- app/page.tsx
+++ app/page.tsx
@@ -3,33 +3,88 @@
 import Image from 'next/image';
 import Spline from '@splinetool/react-spline';
 import { Horse, Heart, Cube } from '@phosphor-icons/react';
+import { motion, spring } from 'framer-motion';
+import { useState } from 'react';
 
 export default function Home() {
+  const [isExpanded, setIsExpanded] = useState(false);
 
   function onScroll() {
     //console.log('onScroll');
   }
 
+  const containerVariants = {
+    compact: {
+      padding: "2rem"
+    },
+    expanded: {
+      padding: 0
+    },
+  }
+
+  const cardVariants = {
+    compact: {
+      borderRadius: "1rem",
+    },
+    expanded: {
+      borderRadius: "0rem",
+    },
+  }
+
+  const onClick = () => {
+    setIsExpanded(!isExpanded);
+  }
+
   return (
-    <main className="flex flex-col items-center justify-between bg-[#5B5540]">
-      <div className="w-full h-[90vh] p-8 flex flex-col gap-6">
+    <main className="flex flex-col items-center bg-[#5B5540] gap-10 h-screen snap-y overflow-y-scroll">
 
-        <div className="w-full basis-5/6 items-center justify-between font-mono text-sm overflow-hidden rounded-2xl">
+      <motion.section className="w-full h-[90vh] shrink-0 flex flex-col gap-6 snap-center snap-always snap-mandatory"
+      layout
+      variants={containerVariants}
+      animate={isExpanded ? "expanded" : "compact"}
+      transition={spring}>
+
+        <motion.div
+        className="w-full basis-5/6 items-center justify-between font-mono text-sm overflow-hidden rounded-2xl"
+        layout
+        variants={cardVariants}
+        animate={isExpanded ? "expanded" : "compact"}
+        transition={spring}>
           <Spline onScroll={onScroll} scene="https://prod.spline.design/vOTSy200mmfodOdw/scene.splinecode"/>
-        </div>
+        </motion.div>
 
-        <div className='flex flex-col basis-1/6 px-6 pb-10 z-10 gap-1'>
+        <div className='flex flex-col basis-1/6 px-6 pb-10 z-10 gap-2'>
           <h1 className='text-lg font-medium tracking-widest uppercase'>Organon</h1>
           <p className='text-md font-light opacity-80'>Write and learn formal logic like never before, designed for iPhone.</p>
+          <button onClick={onClick} className='text-md w-fit font-normal p-2 px-4 rounded-xl mt-4 tracking-widest underline-offset-4 bg-white text-stone-800'>{"Learn more"}</button>
         </div>
 
-      </div>
+      </motion.section>
 
-      {/* <div className="fixed flex flex-row gap-8 justify-center content-center items-center px-6 z-10 rounded-full h-16 bg-stone-100/10 bottom-12 backdrop-blur drop-shadow-[0px_0px_10px_rgba(0,0,0,0.25)]">
-        <Cube size={36}/>
-        <Horse size={36}/>
-        <Heart size={36}/> 
-      </div> */}
+
+      <motion.section className="w-full h-[90vh] shrink-0 flex flex-col gap-6 snap-center snap-always snap-mandatory"
+      layout
+      variants={containerVariants}
+      animate={isExpanded ? "expanded" : "compact"}
+      transition={spring}>
+
+        <motion.div
+        className="w-full basis-5/6 items-center justify-between font-mono text-sm overflow-hidden rounded-2xl"
+        layout
+        variants={cardVariants}
+        animate={isExpanded ? "expanded" : "compact"}
+        transition={spring}>
+          <Spline onScroll={onScroll} scene="https://prod.spline.design/vOTSy200mmfodOdw/scene.splinecode"/>
+        </motion.div>
+
+        <div className='flex flex-col basis-1/6 px-6 pb-10 z-10 gap-2'>
+          <h1 className='text-lg font-medium tracking-widest uppercase'>Organon</h1>
+          <p className='text-md font-light opacity-80'>Write and learn formal logic like never before, designed for iPhone.</p>
+          <button onClick={onClick} className='text-md w-fit font-normal p-2 px-4 rounded-xl mt-4 tracking-widest underline-offset-4 bg-white text-stone-800'>{"Learn more"}</button>
+        </div>
+
+      </motion.section>
+
     </main>
   )
 }
