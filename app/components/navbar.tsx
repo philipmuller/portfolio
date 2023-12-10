83536ea9fd2af7563b85004772d01633bfb084d0
Philip Müller <philipmuller97@gmail.com>
2023-12-10 18:24:20 +0200

    Make navbar parent explicitly relative

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
 
     return (
         <div className="h-14 flex justify-center pointer-events-none items-center fixed z-50 start-0 end-0 lg:bottom-10">
-            <nav className="flex flex-col pointer-events-auto backdrop-blur-md p-2 px-6 rounded-full shadow-none lg:bg-stone-950/50 lg:shadow-[0_0px_18px_10px_rgba(0,0,0,0.3)]">
+            <nav className="flex relative flex-col pointer-events-auto backdrop-blur-md p-2 px-6 rounded-full shadow-none lg:bg-stone-950/50 lg:shadow-[0_0px_18px_10px_rgba(0,0,0,0.3)]">
                 <div className="flex flex-row gap-16 text-white">
                     <button className={viewingProjects ? "lg:text-stone-800" : "lg:text-stone-20"} onClick={() => setViewingProjects(true)}>Projects</button>
                     <button className={!viewingProjects ? "lg:text-stone-800" : "lg:text-stone-20"} onClick={() => setViewingProjects(false)}>About me</button>
