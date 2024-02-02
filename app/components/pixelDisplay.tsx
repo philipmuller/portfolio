949953fd6b409049ed8b5f167023e932b949f8fa
Philip Müller <philipmuller97@gmail.com>
2024-02-02 18:20:46 +0200

    Rewrite pixelDisplay to use framer motion exlusively

--- app/components/pixelDisplay.tsx
+++ app/components/pixelDisplay.tsx
@@ -1,13 +1,58 @@
-import { useAnimate } from "framer-motion";
-import Logo from "./logo";
+import { useAnimate, motion } from "framer-motion";
+import { MotionLogo } from "./logo";
 import { useEffect } from "react";
 
-export default function PixelDisplay({loading, params} : {loading?: boolean, params?: { fillSequence?: string[], glowOpacitySequence?: number[], nextAt?: string | number }}) {
-    const [scope, animate] = useAnimate();
+export default function PixelDisplay({
+  loading,
+  params,
+}: {
+  loading?: boolean;
+  params?: {
+    fillSequence?: string[];
+    glowOpacitySequence?: number[];
+    nextAt?: string | number;
+  };
+}) {
+  const [scope, animate] = useAnimate();
 
-    const animationRings = [
+  const logoContainerVariants = {
+    idle: {},
+    hover: {},
+  };
+
+  const logoVariants = {
+    idle: {
+      fill: "#262626",
+    },
+    hover: {
+      fill: "#FFFFFF",
+    },
+  };
+
+  const logoShadowVariants = {
+    idle: {
+      opacity: 0.0,
+    },
+    hover: {
+      opacity: 1.0,
+    },
+  };
+
+  const descriptionVariants = {
+    idle: {
+      opacity: 0.0,
+      y: 0,
+    },
+    hover: {
+      opacity: 1.0,
+      y: -20,
+    },
+  };
+
+  //prettier-ignore
+  const animationRings = [
         [[5, 7]],
-        [[5, 6], [5, 8], [4, 6], [4, 8], [6, 6], [6, 8], [4, 7], [6, 7]],
+        [[5, 6], [5, 8] , [4, 6], [4, 8], [6, 6], [6, 8], [4, 7], [6, 7]],
         [[4, 5], [5, 5], [6, 5], [7, 6], [7, 7], [7, 8], [6, 9], [5, 9], [4, 9], [3, 8], [3, 7], [3, 6]],
         [[4, 4], [5, 4], [6, 4], [7, 5], [7, 10], [6, 10], [5, 10], [4, 10], [3, 10], [3, 9], [2, 8], [2, 7], [2, 6], [3, 5]],
         [[3, 3], [4, 3], [5, 3], [6, 3], [7, 3], [7, 11], [6, 11], [5, 11], [4, 11], [3, 11], [2, 10], [1, 9], [1, 8], [1, 7], [1, 6], [1, 5], [2, 4]],
@@ -17,66 +62,95 @@ export default function PixelDisplay({loading, params} : {loading?: boolean, par
         [[1, 0], [0, 1]]
     ];
 
-    let fillSequence = params?.fillSequence ?? ["#404040", "rgb(255 255 255)", "#404040"]; //"rgb(248 113 113)"
-    const glowOpacitySequence = params?.glowOpacitySequence ?? [0, 1, 0];
-    const nextAt = params?.nextAt ?? "-0.26";
+  let fillSequence = params?.fillSequence ?? ["#262626", "#FFFFFF", "#262626"]; //"rgb(248 113 113)"
+  const glowOpacitySequence = params?.glowOpacitySequence ?? [0, 0.7, 0];
+  const nextAt = params?.nextAt ?? "-0.26";
 
-    const animationSequence = [
-        ["svg.ring0", {fill: fillSequence}],
-        ["svg.ring0-glow", {opacity: glowOpacitySequence}, {at: "<"}],
-        ["svg.ring1", {fill: fillSequence}, {at: nextAt}],
-        ["svg.ring1-glow", {opacity: glowOpacitySequence}, {at: "<"}],
-        ["svg.ring2", {fill: fillSequence}, {at: nextAt}],
-        ["svg.ring2-glow", {opacity: glowOpacitySequence}, {at: "<"}],
-        ["svg.ring3", {fill: fillSequence}, {at: nextAt}],
-        ["svg.ring3-glow", {opacity: glowOpacitySequence}, {at: "<"}],
-        ["svg.ring4", {fill: fillSequence}, {at: nextAt}],
-        ["svg.ring4-glow", {opacity: glowOpacitySequence}, {at: "<"}],
-        ["svg.ring5", {fill: fillSequence}, {at: nextAt}],
-        ["svg.ring5-glow", {opacity: glowOpacitySequence}, {at: "<"}],
-        ["svg.ring6", {fill: fillSequence}, {at: nextAt}],
-        ["svg.ring6-glow", {opacity: glowOpacitySequence}, {at: "<"}],
-        ["svg.ring7", {fill: fillSequence}, {at: nextAt}],
-        ["svg.ring7-glow", {opacity: glowOpacitySequence}, {at: "<"}],
-        ["svg.ring8", {fill: fillSequence}, {at: nextAt}],
-        ["svg.ring8-glow", {opacity: glowOpacitySequence}, {at: "<"}],
-    ];
+  const animationSequence = [
+    ["svg.ring0", { fill: fillSequence }],
+    ["svg.ring0-glow", { opacity: glowOpacitySequence }, { at: "<" }],
+    ["svg.ring1", { fill: fillSequence }, { at: nextAt }],
+    ["svg.ring1-glow", { opacity: glowOpacitySequence }, { at: "<" }],
+    ["svg.ring2", { fill: fillSequence }, { at: nextAt }],
+    ["svg.ring2-glow", { opacity: glowOpacitySequence }, { at: "<" }],
+    ["svg.ring3", { fill: fillSequence }, { at: nextAt }],
+    ["svg.ring3-glow", { opacity: glowOpacitySequence }, { at: "<" }],
+    ["svg.ring4", { fill: fillSequence }, { at: nextAt }],
+    ["svg.ring4-glow", { opacity: glowOpacitySequence }, { at: "<" }],
+    ["svg.ring5", { fill: fillSequence }, { at: nextAt }],
+    ["svg.ring5-glow", { opacity: glowOpacitySequence }, { at: "<" }],
+    ["svg.ring6", { fill: fillSequence }, { at: nextAt }],
+    ["svg.ring6-glow", { opacity: glowOpacitySequence }, { at: "<" }],
+    ["svg.ring7", { fill: fillSequence }, { at: nextAt }],
+    ["svg.ring7-glow", { opacity: glowOpacitySequence }, { at: "<" }],
+    ["svg.ring8", { fill: fillSequence }, { at: nextAt }],
+    ["svg.ring8-glow", { opacity: glowOpacitySequence }, { at: "<" }],
+  ];
 
-    useEffect(() => {
-        //@ts-ignore
-        const animationControls = animate(animationSequence, {duration: 1.5, ease: "easeInOut", repeat: Infinity, repeatType: "loop", repeatDelay: 0});
+  useEffect(() => {
+    //@ts-ignore
+    const animationControls = animate(animationSequence, {
+      duration: 1.5,
+      ease: "easeInOut",
+      repeat: Infinity,
+      repeatType: "loop",
+      repeatDelay: 0,
+    });
 
-        if (loading) {
-            animationControls.play();
-        } else {
-            animationControls.stop();
-        }
-    }, [loading]);
+    if (!loading) {
+      animationControls.play();
+    } else {
+      animationControls.stop();
+    }
+  }, [loading]);
 
-    return (
-        <div ref={scope} className='basis-2/5 grid grid-cols-8 gap-6 fill-neutral-700 p-20'>
-                {
-                    Array.from(Array(96)).map((_, index) => {
-                        const x = index % 8;
-                        const y = Math.floor(index / 8);
+  return (
+    <div
+      ref={scope}
+      className="grid basis-2/5 grid-cols-8 gap-6 fill-neutral-800 p-20"
+    >
+      {Array.from(Array(96)).map((_, index) => {
+        const x = index % 8;
+        const y = Math.floor(index / 8);
 
-                        let customClassLabel = "";
-                        animationRings.forEach((ring, ringIndex) => {
-                            if (ring.find((value, _) => (x == value[0] && y == value[1])) != undefined) { customClassLabel = `ring${ringIndex}` }
-                        });
+        let customClassLabel = "";
+        animationRings.forEach((ring, ringIndex) => {
+          if (
+            ring.find((value, _) => x == value[0] && y == value[1]) != undefined
+          ) {
+            customClassLabel = `ring${ringIndex}`;
+          }
+        });
 
-                        return (
-                            <div key={index} className={`relative something w-full h-full p-1 ${(x == 5 && y == 7) ? "fill-white" : ""}`}>
-                                <Logo className={`${customClassLabel} peer w-full h-full hover:scale-125 hover:fill-white duration-200 transition-all`} style={{transform: `rotateZ(${(y+x)*30}deg)`}}/>
-                                <Logo className={`${customClassLabel}-glow pointer-events-none w-full h-full absolute fill-white blur-lg p-1 top-0 left-0 ${(x == 5 && y == 7) ? "opacity-100" : "opacity-0"} peer-hover:opacity-100`} style={{transform: `rotateZ(${(y+x)*30}deg)`}}/>
-                                <div className="pointer-events-none absolute flex flex-row overflow-visible content-center items-end justify-center w-1 h-1 text-sm bg-red-500 transition-all peer-hover:-translate-y-6 left-0 right-0 top-2 ml-auto mr-auto invisible peer-hover:visible z-10">
-                                    <h2 className="w-fit min-w-fit text-sm p-3 bg-stone-800 rounded text-center z-10">Description of the skill</h2>
-                                </div>
-                                
-                            </div>
-                        );
-                    })
-                }  
-            </div>
-    );
+        return (
+          <motion.div
+            variants={logoContainerVariants}
+            initial="idle"
+            whileHover="hover"
+            key={index}
+            className={`relative h-full w-full p-1`}
+          >
+            <MotionLogo
+              className={`${customClassLabel} h-full w-full`}
+              variants={logoVariants}
+              style={{ rotateZ: (y + x) * 30 }}
+            />
+            <MotionLogo
+              className={`${customClassLabel}-glow absolute left-0 top-0 h-full w-full fill-white p-1 blur-lg`}
+              variants={logoShadowVariants}
+              style={{ rotateZ: (y + x) * 30 }}
+            />
+            <motion.div
+              className="pointer-events-none absolute left-0 right-0 top-2 z-10 ml-auto mr-auto flex h-1 w-1 flex-row content-center items-end justify-center overflow-visible text-sm"
+              variants={descriptionVariants}
+            >
+              <h2 className="z-10 w-fit min-w-fit rounded bg-stone-800 p-3 text-center text-sm">
+                Description of the skill
+              </h2>
+            </motion.div>
+          </motion.div>
+        );
+      })}
+    </div>
+  );
 }
--- app/components/projectCard.tsx
+++ app/components/projectCard.tsx
@@ -1,72 +1,115 @@
 "use client";
 
-import { motion } from 'framer-motion';
-import { useEffect, useState } from 'react';
-import Spline from '@splinetool/react-spline';
-import Image from 'next/image';
-import { ProjectInfo } from '../model/project-info';
-import { Breakpoint } from '../hooks/useBreakpoints';
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import Spline from "@splinetool/react-spline";
+import Image from "next/image";
+import { ProjectInfo } from "../model/project-info";
+import { Breakpoint } from "../hooks/useBreakpoints";
 
-export default function ProjectCard({ projectInfo, breakpoint, peek, scrollOffset, onClick, className } : { 
-    projectInfo: ProjectInfo, 
-    breakpoint: Breakpoint,
-    peek?: number, 
-    scrollOffset?: number, 
-    onClick?: (id: string) => void ,
-    className?: string,
+export default function ProjectCard({
+  projectInfo,
+  breakpoint,
+  peek,
+  scrollOffset,
+  onClick,
+  className,
+}: {
+  projectInfo: ProjectInfo;
+  breakpoint: Breakpoint;
+  peek?: number;
+  scrollOffset?: number;
+  onClick?: (id: string) => void;
+  className?: string;
 }) {
+  const isMobile = breakpoint <= Breakpoint.md;
 
-    const isMobile = breakpoint <= Breakpoint.md;
+  const [renderIsVisible, setRenderIsVisible] = useState(false);
 
-    const [renderIsVisible, setRenderIsVisible] = useState(false);
+  const color = !isMobile ? "#20202000" : projectInfo.color ?? "#202020";
 
-    const color = !isMobile ? "#20202000" : projectInfo.color ?? "#202020";
+  //console.log("Title: " + projectInfo.title + " breakpoint: " + breakpoint + " color: " + color);
 
-    //console.log("Title: " + projectInfo.title + " breakpoint: " + breakpoint + " color: " + color);
+  const peekOffset = () => {
+    if (
+      peek != undefined &&
+      scrollOffset != undefined &&
+      scrollOffset <= peek
+    ) {
+      return -peek + scrollOffset;
+    }
 
-    const peekOffset = () => {
-        if (peek != undefined && scrollOffset != undefined && scrollOffset <= peek) {
-            return -peek+scrollOffset;
-        }
+    return 0;
+  };
 
-        return 0;
-    };
-
-    return (
-        <motion.section className={`w-[28rem] h-[100svh] lg:h-[80svh] lg:rounded-xl flex flex-col gap-6 snap-center snap-always snap-mandatory px-8 pb-8 pt-16 relative ${className ?? ""}`}
+  return (
+    <motion.section
+      className={`relative flex h-[100svh] w-[28rem] snap-mandatory snap-center snap-always flex-col gap-6 px-8 pb-8 pt-16 lg:h-[80svh] lg:rounded-xl ${className ?? ""}`}
+      layout
+      animate={{ backgroundColor: color }}
+      layoutId={projectInfo.title}
+      onHoverStart={
+        isMobile
+          ? undefined
+          : () => {
+              setRenderIsVisible(() => true);
+            }
+      }
+      onHoverEnd={isMobile ? undefined : () => setRenderIsVisible(() => false)}
+    >
+      <motion.div
+        onViewportEnter={() => {
+          if (isMobile) {
+            setRenderIsVisible(() => true);
+          }
+        }}
         layout
-        animate={{backgroundColor: color}}
-        layoutId={projectInfo.title}
-        onHoverStart={isMobile ? undefined : () => {setRenderIsVisible(() => true)}}
-        onHoverEnd={isMobile ? undefined : () => setRenderIsVisible(() => false)}
+        layoutId={projectInfo.title + "cover"}
+        animate={{ y: peekOffset() }}
+        className={`relative h-[72%] w-full items-center justify-between overflow-hidden rounded-2xl font-mono text-sm`}
+      >
+        <div className="absolute h-full w-full" />
+        <Image
+          src={projectInfo.coverUrl ?? ""}
+          fill={true}
+          objectFit="cover"
+          alt=""
+          className={`absolute h-full w-full ${renderIsVisible ? "animate-pulse" : ""}`}
+        />
+        {renderIsVisible && projectInfo.sceneUrl != undefined && (
+          <Spline scene={projectInfo.sceneUrl ?? ""} className="relative" />
+        )}
+      </motion.div>
+
+      <div className="z-10 flex basis-1/6 animate-none flex-col gap-1 px-6 transition-none">
+        <motion.h1
+          layout
+          layoutId={projectInfo.title + "title"}
+          onViewportEnter={() => {
+            const metaThemeColor = document.querySelector(
+              "meta[name=theme-color]",
+            );
+            //console.log(metaThemeColor);
+            metaThemeColor?.setAttribute("content", color);
+          }}
+          className="text-lg font-medium uppercase tracking-widest"
         >
-
-            <motion.div 
-            onViewportEnter={() => { if(isMobile) { setRenderIsVisible(() => true)} } } 
-            layout layoutId={projectInfo.title+"cover"} animate={{y: peekOffset()}} 
-            className={`w-full h-[72%] items-center justify-between font-mono text-sm overflow-hidden rounded-2xl relative`}>
-                <div className='w-full h-full absolute'/>
-                <Image src={projectInfo.coverUrl ?? ""} fill={true} objectFit="cover" alt="" className={`w-full h-full absolute ${renderIsVisible ? "animate-pulse" : ""}`}/>
-                {renderIsVisible && projectInfo.sceneUrl != undefined && 
-                <Spline scene={projectInfo.sceneUrl ?? ""} className='relative'/>}
-         
-            </motion.div>
-
-            <div className='flex flex-col basis-1/6 px-6 z-10 gap-1 animate-none transition-none'>
-                <motion.h1
-                layout layoutId={projectInfo.title+"title"}
-                onViewportEnter={() => {
-                    const metaThemeColor = document.querySelector("meta[name=theme-color]");
-                    //console.log(metaThemeColor);
-                    metaThemeColor?.setAttribute("content", color);
-                }}
-                className='text-lg font-medium tracking-widest uppercase'>{projectInfo.title}</motion.h1>
-                <p className='text-sm font-light opacity-80'>{projectInfo.description}</p>
-                <button 
-                onClick={() => { if (onClick != undefined) { onClick(projectInfo.title)} }}
-                className='text-sm w-fit font-normal p-2 px-4 rounded-full -ml-1.5 lg:ml-0 mt-4 underline-offset-4 bg-white text-stone-800 lg:bg-transparent lg:p-0 lg:px-0 lg:text-stone-50 lg:underline'>Learn more</button>
-            </div>
-
-        </motion.section>
-    );
+          {projectInfo.title}
+        </motion.h1>
+        <p className="text-sm font-light opacity-80">
+          {projectInfo.description}
+        </p>
+        <button
+          onClick={() => {
+            if (onClick != undefined) {
+              onClick(projectInfo.title);
+            }
+          }}
+          className="-ml-1.5 mt-4 w-fit rounded-full bg-white p-2 px-4 text-sm font-normal text-stone-800 underline-offset-4 lg:ml-0 lg:bg-transparent lg:p-0 lg:px-0 lg:text-stone-50 lg:underline"
+        >
+          Learn more
+        </button>
+      </div>
+    </motion.section>
+  );
 }
